cc00ef9f34c7fc671133a51aac084edf48c25c46
yuydc <102901839+yuydc@users.noreply.github.com>
2024-05-26 12:48:21 +0300

    Create Password_cracker.py
    A code that cracks passwords by brute force.

---
+++ Password_cracker.py
@@ -0,0 +1,13 @@
+import itertools
+import string
+def guess_password(real):
+    chars = string.ascii_lowercase + string.digits
+    attempts = 0
+    for password_length in range(1, 9):
+        for guess in itertools.product(chars, repeat=password_length):
+            attempts += 1
+            guess = ''.join(guess)
+            if guess == real:
+                return 'password is {}. found in {} guesses.'.format(guess, attempts)
+input=input("Enter password: ")
+print(guess_password(input))
